c9b5d1fdce5d4203e0a3cc77143e6b3854ce9aea
sameer bhatta <sameerbhatta37@gmail.com>
2025-09-29 18:25:02 +0545

    change the tab into screen

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,22 +1,12 @@
-import { Tabs } from "expo-router";
+import { Stack } from "expo-router";
 
 export default function AuthLayout() {
     return (
-        <Tabs
-            screenOptions={{
-                headerShown: false, // hide header
-                tabBarActiveTintColor: "#2563eb", // active color (blue-500)
-                tabBarInactiveTintColor: "gray",  // inactive color
-                tabBarStyle: {
-                    backgroundColor: "white",
-                    borderTopWidth: 1,
-                    borderTopColor: "#e5e7eb",
-                },
-            }}
-        >
-            <Tabs.Screen name="login" options={{ title: "Login" }} />
-            <Tabs.Screen name="signup" options={{ title: "Signup" }} />
-            <Tabs.Screen name="forgot-password" options={{ title: "Forgot" }} />
-        </Tabs>
+        <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="index" />            {/* Get Started */}
+            <Stack.Screen name="login" />
+            <Stack.Screen name="signup" />
+            <Stack.Screen name="forgot-password" />
+        </Stack>
     );
 }
