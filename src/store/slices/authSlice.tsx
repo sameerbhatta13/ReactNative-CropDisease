8bb98379545abcf21d018d164c5d5161fed66b8f
sameer bhatta <sameerbhatta37@gmail.com>
2025-10-05 11:56:11 +0545

    update the authslice with actual api

--- src/store/slices/authSlice.tsx
+++ src/store/slices/authSlice.tsx
@@ -1,9 +1,11 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import api from "../../api/axiosInstance";
 
 interface AuthState {
-    user: null | { id: string; name: string };
+    user: null | { id: string; name: string, email: string };
     token: string | null;
+    setUser: boolean;
     loading: boolean;
     error: string | null;
 }
@@ -11,19 +13,31 @@ interface AuthState {
 const initialState: AuthState = {
     user: null,
     token: null,
+    setUser: false,
     loading: false,
     error: null,
 };
+export const registerUser = createAsyncThunk(
+    "auth/registerUser",
+    async (formData: Record<string, any>, { rejectWithValue }) => {
+        try {
+            const response = await api.post("/auth/register", formData);
+            return response.data; // expect { user, token }
+        } catch (err: any) {
+            return rejectWithValue(err.response.data);
+        }
+    }
+);
 
 // Async thunk for login
 export const loginUser = createAsyncThunk(
     "auth/loginUser",
-    async (credentials: { email: string; password: string }, thunkAPI) => {
+    async (credentials: { email: string; password: string }, { rejectWithValue }) => {
         try {
             const response = await api.post("/auth/login", credentials);
             return response.data; // expect { user, token }
         } catch (err: any) {
-            return thunkAPI.rejectWithValue(err.response.data);
+            return rejectWithValue(err.response.data);
         }
     }
 );
@@ -42,16 +56,39 @@ const authSlice = createSlice({
         builder
             .addCase(loginUser.pending, (state) => {
                 state.loading = true;
+                state.setUser = false
                 state.error = null;
             })
             .addCase(loginUser.fulfilled, (state, action) => {
                 state.loading = false;
-                state.user = action.payload.user;
-                state.token = action.payload.token;
+                state.user = action.payload.data.user;
+                state.setUser = true;
+                state.token = action.payload.data.accessToken;
+
+                AsyncStorage.setItem('token', action.payload.data.accessToken)
             })
             .addCase(loginUser.rejected, (state, action: any) => {
                 state.loading = false;
+                state.setUser = false;
                 state.error = action.payload?.message || "Login failed";
+            })
+            .addCase(registerUser.pending, (state) => {
+                state.loading = true;
+                state.setUser = false;
+                state.error = null;
+            })
+            .addCase(registerUser.fulfilled, (state, action) => {
+                state.loading = false;
+                state.setUser = true;
+                state.user = action.payload.data.user;
+                state.token = action.payload.data.accessToken;
+
+                AsyncStorage.setItem('token', action.payload.data.accessToken)
+            })
+            .addCase(registerUser.rejected, (state, action: any) => {
+                state.loading = false;
+                state.setUser = false
+                state.error = action.payload?.message || "Register failed";
             });
     },
 });
