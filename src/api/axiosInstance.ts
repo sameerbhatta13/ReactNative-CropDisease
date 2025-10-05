a08f273970aedb7e7abe69a1f3c3705921557bdd
sameer bhatta <sameerbhatta37@gmail.com>
2025-10-05 17:58:34 +0545

    update the backend api with the ip address

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,8 +1,10 @@
 
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
+import Constants from 'expo-constants';
 
 // reads from .env
+const BACKEND_URL = Constants.expoConfig?.extra?.EXPO_PUBLIC_API_URL || "http://192.168.1.68:3000/api/";
 
 const api = axios.create({
     // baseURL: BACKEND_URL,
