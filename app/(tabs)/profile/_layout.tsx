51c45828853c4daf005df6d76c53b8b8efb43ce4
sameer bhatta <sameerbhatta37@gmail.com>
2025-10-05 18:01:08 +0545

    make the header shown false

--- app/(tabs)/profile/_layout.tsx
+++ app/(tabs)/profile/_layout.tsx
@@ -3,9 +3,9 @@ import { Stack } from "expo-router";
 export default function ProfileLayout() {
     return (
         <Stack>
-            <Stack.Screen name="index" options={{ title: "Profile" }} />
-            <Stack.Screen name="setting" options={{ title: "Settings" }} />
-            <Stack.Screen name="edit" options={{ title: "Edit Profile" }} />
+            <Stack.Screen name="index" options={{ headerShown: false }} />
+            <Stack.Screen name="setting" options={{ headerShown: false }} />
+            <Stack.Screen name="edit" options={{ headerShown: false }} />
         </Stack>
     );
 }
