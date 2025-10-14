5a1437debba57c8feffd82ad107d79ef3f0c0f4d
sameer bhatta <sameerbhatta37@gmail.com>
2025-10-14 21:03:49 +0545

    added the profiledetials in the stack screen

--- app/(tabs)/profile/_layout.tsx
+++ app/(tabs)/profile/_layout.tsx
@@ -6,6 +6,7 @@ export default function ProfileLayout() {
             <Stack.Screen name="index" options={{ headerShown: false }} />
             <Stack.Screen name="setting" options={{ headerShown: false }} />
             <Stack.Screen name="edit" options={{ headerShown: false }} />
+            <Stack.Screen name="profileDetails" options={{ headerShown: false }} />
         </Stack>
     );
 }
