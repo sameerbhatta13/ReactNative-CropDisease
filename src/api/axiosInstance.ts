07a56379a31e32238993cfb3f280a4530f7a78a3
sameer bhatta <sameerbhatta37@gmail.com>
2025-10-14 21:05:35 +0545

    log the information

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -16,6 +16,7 @@ const api = axios.create({
 
 api.interceptors.request.use(async (config) => {
     const token = await AsyncStorage.getItem('token');
+    console.log('token', token)
     if (token) {
         config.headers.Authorization = `Bearer ${token}`
     }
