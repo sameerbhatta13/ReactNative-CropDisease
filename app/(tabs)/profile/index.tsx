6f09e184127906073075e50f58002e6c443d8b11
sameer bhatta <sameerbhatta37@gmail.com>
2025-10-14 21:04:42 +0545

    fetch the profile in the index file

--- app/(tabs)/profile/index.tsx
+++ app/(tabs)/profile/index.tsx
@@ -1,34 +1,49 @@
-import { RootState } from "@/src/store";
+import { AppDispatch, RootState } from "@/src/store";
+import { getCurrentUser } from "@/src/store/slices/authSlice";
+import { getProfile } from "@/src/store/slices/profileSlice";
 import { MaterialIcons } from "@expo/vector-icons";
+import Constants from 'expo-constants';
 import { useRouter } from "expo-router";
+import { useEffect } from "react";
 import { Image, Text, TouchableOpacity, View } from "react-native";
-import { useSelector } from "react-redux";
-
+import { useDispatch, useSelector } from "react-redux";
 export default function ProfileScreen() {
     const router = useRouter()
+    const dispatch = useDispatch<AppDispatch>()
     const { user } = useSelector((state: RootState) => state.auth)
+    const { profile, loading } = useSelector((state: RootState) => state.profile)
+
+    const apiUrl = Constants.expoConfig?.extra?.EXPO_PUBLIC_API_URL || '';
+
+    console.log('getcurrent user', user)
+    console.log('getcurrent user', apiUrl)
+
+    useEffect(() => {
+        dispatch(getProfile())
+        dispatch(getCurrentUser())
+    }, [])
 
     return (
         <View className="flex-1 bg-white px-5 py-10">
             {/* Profile Header */}
             <View className="items-center mb-8">
                 <Image
-                    source={{ uri: "https://i.pravatar.cc/150?img=12" }}
+                    source={{ uri: `http://192.168.1.68:3000${profile?.avatarUrl}` }}
                     className="w-24 h-24 rounded-full mb-3"
                 />
-                <Text className="text-xl font-semibold">{user?.name}</Text>
-                <Text className="text-gray-500">{user?.email}</Text>
+                <Text className="text-xl font-semibold">{profile?.user?.name}</Text>
+                <Text className="text-gray-500">{profile?.user?.email}</Text>
             </View>
 
             {/* Settings Options */}
             <View className="space-y-4">
-                <TouchableOpacity
+                {/* <TouchableOpacity
                     className="flex-row items-center p-4 rounded-2xl bg-gray-100"
                     onPress={() => router.push("/profile/edit")}
                 >
                     <MaterialIcons name="edit" size={24} color="black" />
                     <Text className="ml-3 text-base">Update Profile</Text>
-                </TouchableOpacity>
+                </TouchableOpacity> */}
 
                 <TouchableOpacity
                     className="flex-row items-center p-4 rounded-2xl bg-gray-100"
