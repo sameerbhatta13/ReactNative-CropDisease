dc3a64055fd3ff14c9ddbc9dbc0bc5b81c9a8941
sameer bhatta <sameerbhatta37@gmail.com>
2025-10-14 21:06:08 +0545

    update the api call for the get current user, fetch user , logout etc

--- src/store/slices/authSlice.tsx
+++ src/store/slices/authSlice.tsx
@@ -41,12 +41,52 @@ export const loginUser = createAsyncThunk(
     async (credentials: { email: string; password: string }, { rejectWithValue }) => {
         try {
             const response = await api.post("/auth/login", credentials);
+            console.log('response', response)
             return response?.data; // expect { user, token }
         } catch (err: any) {
             return rejectWithValue(err.response?.data);
         }
     }
 );
+
+export const getCurrentUser = createAsyncThunk(
+    "auth/getCurrentUser",
+    async (_, { rejectWithValue }) => {
+        try {
+            const response = await api.get("/auth/getCurrentUser");
+            console.log("response", response.data);
+            return response.data; // return only data
+        } catch (err: any) {
+            return rejectWithValue(err.response?.data || { message: "Failed to fetch user" });
+        }
+    }
+);
+
+
+export const forgetPassword = createAsyncThunk(
+    "auth/forgetPassword",
+    async ({ email }: { email: string }, { rejectWithValue }) => {
+        try {
+            const response = await api.post("/auth/forget-password", { email });
+            return response.data;
+        } catch (error: any) {
+            return rejectWithValue(error.response?.data || { message: "Something went wrong" });
+        }
+    }
+);
+
+export const resetPassword = createAsyncThunk(
+    "auth/resetPassword",
+    async ({ token, newPassword, confirmNewPassword }: { token: string, newPassword: string, confirmNewPassword: string }, { rejectWithValue }) => {
+        try {
+            const response = await api.post("/auth/reset-password", { token, newPassword, confirmNewPassword });
+            return response.data;
+        } catch (error: any) {
+            return rejectWithValue(error.response?.data || { message: "Something went wrong" });
+        }
+    }
+);
+
 
 const authSlice = createSlice({
     name: "auth",
@@ -70,6 +110,7 @@ const authSlice = createSlice({
                 state.user = action.payload?.data?.user;
                 state.setUser = true;
                 state.token = action.payload?.data?.accessToken;
+                console.log('user', action.payload?.data?.user)
 
                 AsyncStorage.setItem('token', action?.payload?.data?.accessToken)
             })
@@ -92,6 +133,34 @@ const authSlice = createSlice({
                 AsyncStorage.setItem('token', action.payload.data.accessToken)
             })
             .addCase(registerUser.rejected, (state, action: any) => {
+                state.loading = false;
+                state.setUser = false
+                state.error = action?.payload || "Register failed";
+            })
+            .addCase(getCurrentUser.pending, (state) => {
+                state.loading = true;
+                state.setUser = false;
+                state.error = null;
+            })
+            .addCase(getCurrentUser.fulfilled, (state, action) => {
+
+                const { user, profile, passwordChanged } = action.payload.data;
+
+                // ✅ Check if password was changed
+                if (passwordChanged) {
+                    AsyncStorage.removeItem("token");
+                    state.user = null;
+                    state.token = null;
+                    state.setUser = false;
+                    state.error = "Password changed, please log in again";
+                    return;
+                }
+                state.loading = false;
+                state.setUser = true;
+                state.user = action.payload?.user || action.payload; // depends on backend format
+            })
+
+            .addCase(getCurrentUser.rejected, (state, action: any) => {
                 state.loading = false;
                 state.setUser = false
                 state.error = action?.payload || "Register failed";
